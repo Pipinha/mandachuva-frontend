4308d2146c5ebc8086ebb6d668e37307fce095b9
Eric Teixeira <ertfly@gmail.com>
2022-11-22 18:27:26 -0300

    fix

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -9,11 +9,11 @@ class Home extends Component {
                 <div className="container">
                     <div className="row block-1 align-items-center mt-4">
                         <div className="col-md-6 text-center">
-                            <img src="/assets/img/img-home-1.png" alt="Rainmakr" />
+                            <img src="/assets/img/img-home-1.png" alt="Rainmakr" className="img-fluid" />
                         </div>
                         <div className="col-md-6 d-flex justify-content-center flex-column">
                             <div className="title">BE YOUR BUSINESS MANAGER</div>
-                            <div className="caption mt-4 px-4">Sell your content, get jobs, pitch to brands and be part of the Rain community!</div>
+                            <div className="caption mt-4 px-4">Sell your content, get jobs, pitch to<br/> brands and be part of the Rain community!</div>
                             <div className="box-button">
                                 <Link to={'/'} className="btn">Be a Rainmakr</Link>
                             </div>
