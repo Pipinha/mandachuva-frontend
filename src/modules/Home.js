be3e40b1bf0c8ee0edac4e482746013943680e32
Eric Teixeira <ertfly@gmail.com>
2022-11-22 23:22:23 -0300

    fix

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -7,26 +7,35 @@ class Home extends Component {
         return (
             <>
                 <div className="container">
-                    <div className="row block-1 align-items-center mt-4">
+                    <div className="row block-1 align-items-center mt-6">
                         <div className="col-md-6 text-center">
                             <img src="/assets/img/img-home-1.png" alt="Rainmakr" className="img-fluid" />
                         </div>
                         <div className="col-md-6 d-flex justify-content-center flex-column">
-                            <div className="title">BE YOUR BUSINESS MANAGER</div>
+                            <div className="title text-center">BE YOUR BUSINESS MANAGER</div>
                             <div className="caption mt-4 px-4">Sell your content, get jobs, pitch to<br /> brands and be part of the Rain community!</div>
                             <div className="box-button text-center mt-4">
                                 <Link to={'/'} className="btn btn-grad-a with-shadow">Be a Rainmakr</Link>
                             </div>
                         </div>
                     </div>
-                    <div className="row block-2 align-items-center">
+                    <div className="row block-2 align-items-center mt-6">
                         <div className="col-md-6">
-                            <div className="box-brand d-flex flex-column justify-content-center">
-                                <div className="title">ARE YOU A BRAND?</div>
+                            <div className="box-brand d-flex flex-column justify-content-center align-items-center">
+                                <div className="title text-center">ARE YOU A BRAND?</div>
                                 <div className="caption mt-3">Easy, simple and fast!</div>
                                 <div className="box-button text-center mt-3">
                                     <Link to={'/'} className="btn btn-a with-shadow text-white">Check what you can do</Link>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="d-flex flex-column justify-content-center align-items-center">
+                                <div className="bg-brand">
+                                    <img src="/assets/img/bg-block-2.png" alt="Rainmakr" />
+                                    <div className="title-over">CONNECT<br />WITH<br />BRANDS</div>
+                                </div>
+                                <div className="caption adjust">Rainmakr connects creators with brands to access jobs and sell your never seen ready-to-post content, and make money.<br /><br />Make it rain, be a Rainmakr!</div>
                             </div>
                         </div>
                     </div>
