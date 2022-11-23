6e4c068454fb76ce81bbf1be975f1567f0d344d5
Eric Teixeira <ertfly@gmail.com>
2022-11-23 01:28:15 -0300

    add block-3

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
                         </div>
                         <div className="col-md-6 d-flex justify-content-center flex-column">
                             <div className="title text-center">BE YOUR BUSINESS MANAGER</div>
-                            <div className="caption mt-4 px-4">Sell your content, get jobs, pitch to<br /> brands and be part of the Rain community!</div>
+                            <div className="caption text-center mt-4 px-4">Sell your content, get jobs, pitch to<br /> brands and be part of the Rain community!</div>
                             <div className="box-button text-center mt-4">
                                 <Link to={'/'} className="btn btn-grad-a with-shadow">Be a Rainmakr</Link>
                             </div>
@@ -35,8 +35,23 @@ class Home extends Component {
                                     <img src="/assets/img/bg-block-2.png" alt="Rainmakr" />
                                     <div className="title-over">CONNECT<br />WITH<br />BRANDS</div>
                                 </div>
-                                <div className="caption adjust">Rainmakr connects creators with brands to access jobs and sell your never seen ready-to-post content, and make money.<br /><br />Make it rain, be a Rainmakr!</div>
+                                <div className="caption text-center adjust">Rainmakr connects creators with brands to access jobs and sell your never seen ready-to-post content, and make money.<br /><br />Make it rain, be a Rainmakr!</div>
                             </div>
+                        </div>
+                    </div>
+                    <div className="row block-3 mt-6">
+                        <div className="col-md-6 d-flex flex-column justify-content-center align-items-center">
+                            <div className="box-info">
+                                <div className="title-links">RAINLINKS</div>
+                                <div className="caption mt-2">Get your page to share links and receive direct bookings from brands. Set the prices for your content and start selling. Easy, fast, and free.</div>
+                            </div>
+                            <div className="box-info mt-5">
+                                <div className="title-jobs text-right">RAINJOBS</div>
+                                <div className="caption text-right mt-2">Access jobs from over 170 platforms in just one place. 1000s of active creators and jobs with the highest response rates.</div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 box-img">
+                            <img src="/assets/img/img-links-jobs.png" alt="Rainmakr" className="img-fluid" />
                         </div>
                     </div>
                 </div>
