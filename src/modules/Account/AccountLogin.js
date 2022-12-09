f7d66baa16849afafee72825355eaca48246b96c
Eric Teixeira <ertfly@gmail.com>
2022-12-09 19:37:13 -0300

    fix

--- src/modules/Account/AccountLogin.js
+++ src/modules/Account/AccountLogin.js
@@ -28,11 +28,11 @@ class AccountLogin extends Component {
                             <div className="logo d-flex align-items-center justify-content-center">
                                 <img src="/assets/img/logo-login.png" alt="Rainmaker" />
                             </div>
-                            <div className="mt-4">
+                            <div className="mt-3">
                                 <h2 className="text-grad-hot-pink">SIGN IN</h2>
                                 <h4 className="text-white mt-3">AND START TO ...</h4>
                             </div>
-                            <div className="mt-5">
+                            <div className="mt-3">
                                 <form onSubmit={e => { e.preventDefault(); this.loginSubmit() }}>
                                     <div>
                                         <input type="text" className="form-control with-label" value={this.state.login} onChange={(e) => this.setState({ login: e.target.value })} placeholder="Login" />
@@ -41,19 +41,19 @@ class AccountLogin extends Component {
                                         <input type="password" className="form-control with-label" value={this.state.pass} onChange={(e) => this.setState({ pass: e.target.value })} placeholder="Password" />
                                     </div>
                                     <div className="text-right">
-                                        <Link to={'/'} className="text-pitaya">Forgot password ?</Link>
+                                        <Link to={'/account/forgot'} className="text-pitaya">Forgot password ?</Link>
                                     </div>
                                     <div className="mt-3">
                                         <button type="submit" className="btn btn-a btn-block with-shadow">Login</button>
                                     </div>
                                     <h5 className="text-gray-30 text-center mt-4">OR CONTINUE WITH</h5>
                                     <div className="d-flex justify-content-center mt-4">
-                                        <Link to={'/'}><img src="/assets/img/ico-facebook.png" alt="Rainmakr" /></Link>
-                                        <Link to={'/'} className="ml-2"><img src="/assets/img/ico-google.png" alt="Rainmakr" /></Link>
+                                        <a href="https://facebook.com.br"><img src="/assets/img/ico-facebook.png" alt="Rainmakr" /></a>
+                                        <a href="https://google.com.br" className="ml-2"><img src="/assets/img/ico-google.png" alt="Rainmakr" /></a>
                                     </div>
                                     <div className="line-sep bg-steelo-l2 mt-4">&nbsp;</div>
                                     <div>
-                                        <h6 className="text-white mt-4">IF YOU DON’T HAVE AN ACCOUNT <Link className="h6 text-blueberry-l2">REGISTER HERE !</Link></h6>
+                                        <h6 className="text-white mt-4">IF YOU DON’T HAVE AN ACCOUNT <Link to={'/register'} className="h6 text-blueberry-l2">REGISTER HERE !</Link></h6>
                                     </div>
                                 </form>
                             </div>
