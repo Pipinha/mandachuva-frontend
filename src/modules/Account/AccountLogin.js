39d77ec47aff4d96c7b4f288922b4439285c30af
Eric Teixeira <ertfly@gmail.com>
2022-12-09 18:48:25 -0300

    fix

--- src/modules/Account/AccountLogin.js
+++ src/modules/Account/AccountLogin.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 class AccountLogin extends Component {
     constructor(props) {
@@ -9,7 +9,16 @@ class AccountLogin extends Component {
         props.handlerHeader({
             page: 'account-login'
         })
+
+        this.state = {
+            login: '',
+            pass: '',
+        }
     }
+    loginSubmit() {
+
+    }
+
     render() {
         return (
             <>
@@ -19,9 +28,25 @@ class AccountLogin extends Component {
                             <div className="logo d-flex align-items-center justify-content-center">
                                 <img src="/assets/img/logo-login.png" alt="Rainmaker" />
                             </div>
-                            <div>
+                            <div className="mt-4">
                                 <h2 className="text-grad-hot-pink">SIGN IN</h2>
                                 <h4 className="text-white mt-3">AND START TO ...</h4>
+                            </div>
+                            <div className="mt-5">
+                                <form onSubmit={e => { e.preventDefault(); this.loginSubmit() }}>
+                                    <div>
+                                        <input type="text" className="form-control with-label" value={this.state.login} onChange={(e) => this.setState({ login: e.target.value })} placeholder="Login" />
+                                    </div>
+                                    <div className="mt-3">
+                                        <input type="password" className="form-control with-label" value={this.state.pass} onChange={(e) => this.setState({ pass: e.target.value })} placeholder="Password" />
+                                    </div>
+                                    <div className="text-right">
+                                        <Link to={'/'} className="text-pitaya">Forgot password ?</Link>
+                                    </div>
+                                    <div className="mt-3">
+                                        <button type="submit" className="btn btn-a btn-block">Login</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
