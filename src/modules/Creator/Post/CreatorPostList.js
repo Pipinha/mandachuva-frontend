e563056797b76db238a2cf96c2d5df9070fb96c0
Eric Teixeira <ertfly@gmail.com>
2022-12-19 17:14:34 -0300

    fix

--- src/modules/Creator/Post/CreatorPostList.js
+++ src/modules/Creator/Post/CreatorPostList.js
@@ -79,10 +79,8 @@ class CreatorPostList extends Component {
                                     </div>
                                     <div className="store d-flex align-items-center justify-content-center">
                                         <span className="a text-white mr-1">Store:</span>
-                                        <span className="b text-white">On</span>
-                                        <div className="switch ml-2">
-                                            <Switch onChange={this.changeStore} checked={this.state.store} offColor="#3CE9FF" onColor="#3CE9FF" offHandleColor="#00AFC6" onHandleColor="#00AFC6" uncheckedIcon={false} checkedIcon={false} checkedHandleIcon={<Icon icon="material-symbols:check" width={18} height={18} color="#ffffff" style={{marginTop:'-6px',marginLeft:'1px'}} />} uncheckedHandleIcon={<Icon icon="material-symbols:close" width={18} height={18} color="#ffffff" style={{marginTop:'-6px',marginLeft:'1px'}} />} handleDiameter={20} />
-                                        </div>
+                                        <span className="b text-white mr-2">On</span>
+                                        <Switch onChange={this.changeStore} checked={this.state.store} offColor="#3CE9FF" onColor="#3CE9FF" offHandleColor="#00AFC6" onHandleColor="#00AFC6" uncheckedIcon={false} checkedIcon={false} checkedHandleIcon={<Icon icon="material-symbols:check" width={18} height={18} color="#ffffff" style={{marginTop:'-6px',marginLeft:'1px'}} />} uncheckedHandleIcon={<Icon icon="material-symbols:close" width={18} height={18} color="#ffffff" style={{marginTop:'-6px',marginLeft:'1px'}} />} handleDiameter={20} />
                                     </div>
                                 </div>
                             </div>
