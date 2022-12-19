053c5fdd9fed87961d03972afa124b904a9c86ca
Eric Teixeira <ertfly@gmail.com>
2022-12-19 04:47:32 -0300

    fix

--- src/modules/Creator/CreatorSetting.js
+++ src/modules/Creator/CreatorSetting.js
@@ -1,6 +1,7 @@
+import { Icon } from "@iconify/react";
 import { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 class CreatorSetting extends Component {
     constructor(props) {
@@ -11,7 +12,7 @@ class CreatorSetting extends Component {
         })
 
         this.state = {
-            
+
         }
     }
     submit() {
@@ -33,16 +34,36 @@ class CreatorSetting extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="container mt-4">
-                    <div className="row">
+                <div className="container">
+                    <div className="back-to mt-3">
+                        <Link onClick={_ => window.history.back()} className="text-blueberry"><Icon icon="material-symbols:arrow-back-ios-new-rounded" width={14} height={14} /> Back to Dashboard</Link>
+                    </div>
+                    <div className="row mt-3">
                         <div className="col-md-3">
-                            <div className="card-custom">
-
+                            <div className="card-custom p-3">
+                                <div className="simple">
+                                    <div className="title2 text-white">SETTINGS MENU</div>
+                                    <div className="sep mt-2"></div>
+                                    <div className="item-menu active">
+                                        <Link to={'/creator/setting'}><Icon icon="ic:baseline-person-outline" width={14} height={14} /> Profile settings</Link>
+                                    </div>
+                                    <div className="sep mt-2"></div>
+                                    <div className="item-menu">
+                                        <Link to={'/creator/setting/social'}><Icon icon="mdi:instagram" width={14} height={14} /> Social Media</Link>
+                                    </div>
+                                    <div className="sep mt-2"></div>
+                                    <div className="item-menu">
+                                        <Link to={'/creator/setting/privacy'}><Icon icon="ic:outline-privacy-tip" width={14} height={14} /> Privacy and security</Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="col-md-9">
-                            <div className="card-custom">
-                                
+                            <div className="card-custom p-3">
+                                <div className="simple">
+                                    <div className="title2 text-white">PROFILE SETTINGS</div>
+                                    <div className="sep mt-2"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
