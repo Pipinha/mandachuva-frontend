735748b890e45f41000a0da44fa1ba0f0ba00946
Eric Teixeira <ertfly@gmail.com>
2023-01-09 19:34:04 -0300

    fix

--- src/modules/Register/Register.js
+++ src/modules/Register/Register.js
@@ -59,15 +59,7 @@ class Register extends Component {
                             </div>
                             <div className="mt-4">
                                 <form onSubmit={e => { e.preventDefault(); this.submit() }}>
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <label className="text-white d-flex align-items-center"><input type="radio" name="profileId" value="1" checked={this.state.profileId === PROFILE_CREATOR} className="mr-2" onChange={(e)=>this.setState({profileId: parseInt(e.target.value)})} /> I'm creator</label>
-                                        </div>
-                                        <div className="col-md-6 d-flex justify-content-end">
-                                            <label className="text-white d-flex align-items-center"><input type="radio" name="profileId" value="2" checked={this.state.profileId === PROFILE_COMPANY} className="mr-2" onChange={(e)=>this.setState({profileId: parseInt(e.target.value)})} /> I'm company</label>
-                                        </div>
-                                    </div>
-                                    <div className="mt-3">
+                                    <div>
                                         <input type="text" className="form-control with-label" value={this.state.firstname} onChange={(e) => this.setState({ firstname: e.target.value })} placeholder="First Name" />
                                     </div>
                                     <div className="mt-3">
@@ -77,10 +69,15 @@ class Register extends Component {
                                         <input type="email" className="form-control with-label" value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })} placeholder="Email" />
                                     </div>
                                     <div className="mt-3">
-                                        <input type="text" className="form-control with-label" placeholder="Enter a Location" />
-                                    </div>
-                                    <div className="mt-3">
                                         <input type="text" className="form-control with-label" value={this.state.phone} onChange={(e) => this.setState({ phone: e.target.value })} placeholder="Phone" />
+                                    </div>
+                                    <div className="row mt-3">
+                                        <div className="col-md-6">
+                                            <label className="text-white d-flex align-items-center radio-profile"><input type="radio" name="profileId" value="1" checked={this.state.profileId === PROFILE_CREATOR} className="mr-2" onChange={(e)=>this.setState({profileId: parseInt(e.target.value)})} /> I'm creator</label>
+                                        </div>
+                                        <div className="col-md-6 d-flex justify-content-end">
+                                            <label className="text-white d-flex align-items-center radio-profile"><input type="radio" name="profileId" value="2" checked={this.state.profileId === PROFILE_COMPANY} className="mr-2" onChange={(e)=>this.setState({profileId: parseInt(e.target.value)})} /> I'm company</label>
+                                        </div>
                                     </div>
                                     <div className="mt-3">
                                         <input type="password" className="form-control with-label" value={this.state.pass} onChange={(e) => this.setState({ pass: e.target.value })} placeholder="Create a password" />
